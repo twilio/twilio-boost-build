96c87e657f9ed306ebc4a890429d4bd09d83af7c
Ryan C. Payne <rpayne@twilio.com>
2020-12-11 12:42:02 -0500

    Revert "I’m just going to go and cry now."
    This reverts commit a4f985d5e452d84534936aba26e5f4fa4c9fef4e.

--- boost.sh
+++ boost.sh
@@ -890,10 +890,10 @@ buildBoost_iOS()
 
         ./b2 $THREADS --build-dir=iphonesim-build --stagedir=iphonesim-build/stage \
             --prefix="$OUTPUT_DIR" \
-            --libdir="$OUTPUT_DIR/lib/$VARIANT/iphonesimulator" \
+            --libdir="$OUTPUT_DIR/lib/$VARIANT/fat-x86" \
             toolset=darwin-${IOS_SDK_VERSION}~iphonesim \
             variant=$VARIANT abi=sysv address-model=32_64 architecture=x86 binary-format=mach-o \
-            target-os=iphone threading=multi optimization=speed link=static \
+            target-os=iphone architecture=x86 threading=multi optimization=speed link=static \
             cxxflags="${CXX_FLAGS} ${CPPSTD} -stdlib=libc++" linkflags="-stdlib=libc++" \
             macosx-version=iphonesim-${IOS_SDK_VERSION} \
             install >> "${OUTPUT_DIR}/iphone-build.log" 2>&1
